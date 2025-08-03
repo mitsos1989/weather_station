16b280a1b00619d39593a7680d35b2cc5373a93d
mitsos1989 <dimitrism@geo.auth.gr>
2025-08-03 16:01:47 +0300

    dfsdfsd

--- auto_telegram.sh
+++
@@ -1,6 +0,0 @@
-while true; do
-  echo "Starting Telegram notifications"
-  python3 notif_telegram.py
-  echo "Το script σταμάτησε με κωδικό εξόδου $?. Επανεκκίνηση σε 10 δευτερόλεπτα..."
-  sleep 10
-done
